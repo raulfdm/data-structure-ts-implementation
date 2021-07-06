c5767aa240b0c8a498b53f9b13d68d644e3c4c85
Raul Melo <melo.raulf@gmail.com>
2021-07-06 07:59:05 +0200

    feat: implement doubly linked list

--- js-version/DoublyLinkedList.js
+++ js-version/DoublyLinkedList.js
@@ -1,27 +1,7 @@
-interface Node<T> {
-  element: T;
-  next: Node<T> | null;
-  prev: Node<T> | null;
-}
-
-export interface DoublyLinkedList<T> {
-  append(element: T): void;
-  indexOf(element: T): number;
-  insertAt(position: number, element: T): boolean;
-  remove(element: T): null | T;
-  removeAt(position: number): null | T;
-  toString(): string;
-  head: Node<T> | null;
-  tail: Node<T> | null;
-  isEmpty(): boolean;
-  size(): number;
-}
-
-export function DoublyLinkedListFactory<T>(): DoublyLinkedList<T> {
+export function DoublyLinkedListFactory() {
   let length = 0;
-  let head: Node<T> | null = null;
-  let tail: Node<T> | null = null;
-
+  let head = null;
+  let tail = null;
   return {
     append,
     indexOf,
@@ -38,70 +18,53 @@ export function DoublyLinkedListFactory<T>(): DoublyLinkedList<T> {
     },
     size,
   };
-
-  function append(element: T) {
-    const node: Node<T> = {
+  function append(element) {
+    const node = {
       element,
       next: null,
       prev: null,
     };
-
     if (head === null) {
       head = node;
       tail = node;
     } else {
       let currentElement = head;
-
       while (currentElement.next) {
         currentElement = currentElement.next;
       }
-
       // Replace "null" with the new node
       currentElement.next = node;
-
       node.prev = currentElement;
-
       // Update tail with node because append will be always to the end
       tail = node;
     }
-
     length++;
   }
-
-  function indexOf(element: T) {
+  function indexOf(element) {
     let currentNode = head;
     let index = 0;
-
     while (currentNode) {
       if (element === currentNode.element) {
         return index;
       }
-
       index++;
       currentNode = currentNode.next;
     }
-
     return -1;
   }
-
-  function insertAt(position: number, element: T) {
+  function insertAt(position, element) {
     const isPositionInTheRange = position > -1 && position <= length;
-
     if (!isPositionInTheRange) {
       return false;
     }
-
-    const node: Node<T> = {
+    const node = {
       element,
       next: null,
       prev: null,
     };
-
     const isHeadPosition = position === 0;
     const isTailPosition = position === length;
-
     let currentNode = head;
-
     if (isHeadPosition) {
       /**
        * This handles the case where the list is empty and
@@ -113,101 +76,78 @@ export function DoublyLinkedListFactory<T>(): DoublyLinkedList<T> {
         /**
          * ensure: node(new head) <-> currentNode (old head)
          */
-        currentNode!.prev = node;
+        currentNode.prev = node;
         node.next = currentNode;
       }
-
       /**
        * For both cases node needs to be the new head
        */
       head = node;
     } else if (isTailPosition) {
-      (tail as Node<T>).next = node;
+      tail.next = node;
       node.prev = tail;
       tail = node;
     } else {
-      let previousNode: Node<T> | null = null;
+      let previousNode = null;
       let index = 0;
-
       while (index++ < position) {
-        previousNode = currentNode as Node<T>;
-        currentNode = currentNode!.next;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
       }
-
-      previousNode!.next = node;
-      currentNode!.prev = node;
-
+      previousNode.next = node;
+      currentNode.prev = node;
       node.prev = previousNode;
       node.next = currentNode;
     }
-
     length++;
-
     return true;
   }
-
-  function removeAt(position: number) {
+  function removeAt(position) {
     const isPositionInTheRange = position > -1 && position < length;
-
     if (!isPositionInTheRange) {
       return null;
     }
-
     let currentNode = head;
-
     const isHeadPosition = position === 0;
     const isTailPosition = position === length - 1;
-
     if (isHeadPosition) {
-      head = (currentNode as Node<T>).next;
+      head = currentNode.next;
     } else if (isTailPosition) {
       currentNode = tail;
-      tail = (currentNode as Node<T>).prev;
+      tail = currentNode.prev;
     } else {
       let index = 0;
-      let previousNode: Node<T> | null = null;
-
+      let previousNode = null;
       while (index++ < position) {
         previousNode = currentNode;
-        currentNode = (currentNode as Node<T>).next;
+        currentNode = currentNode.next;
       }
-
       /**
        * currentNode will only be null when I'm removing the last element
        * but this case is being handled by `isTailPosition` case
        */
-      (currentNode!.next as Node<T>).prev = previousNode;
-      (previousNode as Node<T>).next = (currentNode as Node<T>).next;
+      currentNode.next.prev = previousNode;
+      previousNode.next = currentNode.next;
     }
-
     length--;
-
-    return (currentNode as Node<T>).element;
+    return currentNode.element;
   }
-
-  function remove(element: T) {
+  function remove(element) {
     const elementIndex = indexOf(element);
-
     return removeAt(elementIndex);
   }
-
   function toString() {
     let result = '';
     let current = head;
-
     while (current) {
       result += `${current.element}${current.next ? ', ' : ''}`;
-
       current = current.next;
     }
-
     return result;
   }
-
   function isEmpty() {
     return length === 0;
   }
-
   function size() {
     return length;
   }
--- src/__tests__/DoublyLinkedList.test.ts
+++ src/__tests__/DoublyLinkedList.test.ts
@@ -1,6 +1,6 @@
 import { DoublyLinkedListFactory, DoublyLinkedList } from '../DoublyLinkedList';
 
-describe('LinkedListFactory', () => {
+describe('fn: DoublyLinkedListFactory', () => {
   let doublyLinkedList: DoublyLinkedList<any>;
 
   beforeEach(() => {
@@ -104,140 +104,227 @@ describe('LinkedListFactory', () => {
       });
     });
 
-    // it('inserts element in a specific position and returns true', () => {
-    //   doublyLinkedList.append(10);
-    //   doublyLinkedList.append(20);
-    //   doublyLinkedList.append(30);
+    it('inserts element in a specific position and returns true', () => {
+      doublyLinkedList.append(10);
+      doublyLinkedList.append(20);
+      doublyLinkedList.append(30);
 
-    //   expect(doublyLinkedList.size()).toBe(3);
-    //   expect(doublyLinkedList.toString()).toBe('10, 20, 30');
+      expect(doublyLinkedList.size()).toBe(3);
+      expect(doublyLinkedList.toString()).toBe('10, 20, 30');
 
-    //   expect(doublyLinkedList.insertAt(1, 60)).toBe(true);
-    //   expect(doublyLinkedList.toString()).toBe('10, 60, 20, 30');
-    //   expect(doublyLinkedList.size()).toBe(4);
+      expect(doublyLinkedList.insertAt(1, 60)).toBe(true);
+      expect(doublyLinkedList.toString()).toBe('10, 60, 20, 30');
+      expect(doublyLinkedList.size()).toBe(4);
 
-    //   expect(doublyLinkedList.insertAt(0, 'test')).toBe(true);
-    //   expect(doublyLinkedList.toString()).toBe('test, 10, 60, 20, 30');
-    //   expect(doublyLinkedList.size()).toBe(5);
-    // });
+      expect(doublyLinkedList.insertAt(0, 'test')).toBe(true);
+      expect(doublyLinkedList.toString()).toBe('test, 10, 60, 20, 30');
+      expect(doublyLinkedList.size()).toBe(5);
+    });
 
-    // it('returns false if the position does not exist in the list', () => {
-    //   doublyLinkedList.append(10);
-    //   expect(doublyLinkedList.size()).toBe(1);
+    it('returns false if the position does not exist in the list', () => {
+      doublyLinkedList.append(10);
+      expect(doublyLinkedList.size()).toBe(1);
 
-    //   expect(doublyLinkedList.insertAt(-1, 60)).toBe(false);
-    //   expect(doublyLinkedList.insertAt(3, 60)).toBe(false);
+      expect(doublyLinkedList.insertAt(-1, 60)).toBe(false);
+      expect(doublyLinkedList.insertAt(3, 60)).toBe(false);
 
-    //   expect(doublyLinkedList.toString()).toBe('10');
-    //   expect(doublyLinkedList.size()).toBe(1);
-    // });
+      expect(doublyLinkedList.toString()).toBe('10');
+      expect(doublyLinkedList.size()).toBe(1);
+    });
   });
 
-  // describe('.removeAt()', () => {
-  //   it('returns null if position does not exist in the list', () => {
-  //     doublyLinkedList.append(30);
-  //     doublyLinkedList.append(10);
+  describe('.removeAt()', () => {
+    it('returns null if position does not exist in the list', () => {
+      doublyLinkedList.append(30);
+      doublyLinkedList.append(10);
 
-  //     expect(doublyLinkedList.size()).toBe(2);
+      expect(doublyLinkedList.size()).toBe(2);
 
-  //     expect(doublyLinkedList.removeAt(-1)).toBe(null);
-  //     expect(doublyLinkedList.removeAt(4)).toBe(null);
-  //     expect(doublyLinkedList.size()).toBe(2);
-  //   });
+      expect(doublyLinkedList.removeAt(-1)).toBe(null);
+      expect(doublyLinkedList.removeAt(4)).toBe(null);
+      expect(doublyLinkedList.size()).toBe(2);
+    });
 
-  //   it('returns null if list is empty', () => {
-  //     expect(doublyLinkedList.size()).toBe(0);
-  //     expect(doublyLinkedList.removeAt(0)).toBe(null);
-  //     expect(doublyLinkedList.size()).toBe(0);
-  //   });
+    it('returns null if the list is empty', () => {
+      expect(doublyLinkedList.size()).toBe(0);
+      expect(doublyLinkedList.removeAt(0)).toBe(null);
+      expect(doublyLinkedList.size()).toBe(0);
+    });
 
-  //   describe('head case', () => {
-  //     it('returns the element and remove it from the list (single element)', () => {
-  //       const element = 10;
+    describe('head case', () => {
+      it('returns the element and remove it from the list (single element)', () => {
+        const expectedElement = 10;
 
-  //       doublyLinkedList.append(element);
+        doublyLinkedList.append(expectedElement);
 
-  //       expect(doublyLinkedList.size()).toBe(1);
-  //       expect(doublyLinkedList.toString()).toBe('10');
+        expect(doublyLinkedList.size()).toBe(1);
 
-  //       expect(doublyLinkedList.removeAt(0)).toBe(element);
-  //       expect(doublyLinkedList.toString()).toBe('');
-  //       expect(doublyLinkedList.size()).toBe(0);
-  //     });
+        const removedElement = doublyLinkedList.removeAt(0);
 
-  //     it('returns the element and remove it from the list (multi element)', () => {
-  //       const element = 10;
+        expect(removedElement).toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(0);
+      });
 
-  //       doublyLinkedList.append(element);
-  //       doublyLinkedList.append(30);
-  //       doublyLinkedList.append(60);
+      it('returns the element and remove it from the list (multi element list)', () => {
+        const expectedElement = 10;
 
-  //       expect(doublyLinkedList.toString()).toBe('10, 30, 60');
-  //       expect(doublyLinkedList.size()).toBe(3);
+        doublyLinkedList.append(expectedElement);
+        doublyLinkedList.append(30);
+        doublyLinkedList.append(60);
 
-  //       expect(doublyLinkedList.removeAt(0)).toBe(element);
-  //       expect(doublyLinkedList.toString()).toBe('30, 60');
-  //       expect(doublyLinkedList.size()).toBe(2);
-  //     });
-  //   });
+        expect(doublyLinkedList.toString()).toBe('10, 30, 60');
+        expect(doublyLinkedList.size()).toBe(3);
 
-  //   it('returns the element and remove it from the list', () => {
-  //     const element = -30;
+        const removedElement = doublyLinkedList.removeAt(0);
 
-  //     doublyLinkedList.append(10);
-  //     doublyLinkedList.append(100);
-  //     doublyLinkedList.append(element);
-  //     doublyLinkedList.append(50);
+        expect(removedElement).toBe(expectedElement);
+        expect(doublyLinkedList.head.element).not.toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(2);
+      });
+    });
 
-  //     expect(doublyLinkedList.toString()).toBe('10, 100, -30, 50');
-  //     expect(doublyLinkedList.size()).toBe(4);
+    describe('tail case', () => {
+      it('returns the element and remove it from the list (single element)', () => {
+        const expectedElement = 998;
 
-  //     expect(doublyLinkedList.removeAt(2)).toBe(element);
-  //     expect(doublyLinkedList.toString()).toBe('10, 100, 50');
-  //     expect(doublyLinkedList.size()).toBe(3);
-  //   });
-  // });
+        doublyLinkedList.append(expectedElement);
 
-  // describe('.remove()', () => {
-  //   it('returns null if the element to be remove does not exists', () => {
-  //     expect(doublyLinkedList.remove(10)).toBe(null);
-  //   });
+        expect(doublyLinkedList.size()).toBe(1);
 
-  //   it('returns the element itself and remove from the list', () => {
-  //     const element = 10;
+        const removedElement = doublyLinkedList.removeAt(0);
 
-  //     doublyLinkedList.append(-10);
-  //     doublyLinkedList.append(element);
-  //     doublyLinkedList.append(60);
+        expect(removedElement).toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(0);
+      });
 
-  //     expect(doublyLinkedList.toString()).toBe('-10, 10, 60');
-  //     expect(doublyLinkedList.size()).toBe(3);
+      it('returns the element and remove it from the list (multi element list)', () => {
+        const expectedElement = 53;
 
-  //     expect(doublyLinkedList.remove(element)).toBe(element);
+        doublyLinkedList.append(30);
+        doublyLinkedList.append(60);
+        doublyLinkedList.append(expectedElement);
 
-  //     expect(doublyLinkedList.toString()).toBe('-10, 60');
-  //     expect(doublyLinkedList.size()).toBe(2);
-  //   });
-  // });
+        expect(doublyLinkedList.tail.element).toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(3);
 
-  // describe('.isEmpty()', () => {
-  //   it('returns true if the list does not contain element', () => {
-  //     expect(doublyLinkedList.isEmpty()).toBe(true);
-  //   });
+        const removedElement = doublyLinkedList.removeAt(2);
 
-  //   it('returns false if the list contains element', () => {
-  //     doublyLinkedList.append(10);
-  //     expect(doublyLinkedList.isEmpty()).toBe(false);
-  //   });
-  // });
+        expect(removedElement).toBe(expectedElement);
 
-  // describe('.head', () => {
-  //   it('returns the node containing the first element added', () => {
-  //     const element = 'my name';
+        expect(doublyLinkedList.tail.element).not.toBe(expectedElement);
+        expect(doublyLinkedList.tail.element).toBe(60);
 
-  //     doublyLinkedList.append(element);
+        expect(doublyLinkedList.size()).toBe(2);
+      });
+    });
 
-  //     expect(doublyLinkedList.head.element).toBe(element);
-  //   });
-  // });
+    describe('body case', () => {
+      it('returns the element and remove it from the list', () => {
+        const expectedElement = -30;
+
+        doublyLinkedList.append(10);
+        doublyLinkedList.append(100);
+        doublyLinkedList.append(expectedElement);
+        doublyLinkedList.append(50);
+
+        expect(doublyLinkedList.toString()).toBe('10, 100, -30, 50');
+        expect(doublyLinkedList.size()).toBe(4);
+
+        const removedElement = doublyLinkedList.removeAt(2);
+
+        expect(removedElement).toBe(expectedElement);
+        expect(doublyLinkedList.toString()).toBe('10, 100, 50');
+        expect(doublyLinkedList.size()).toBe(3);
+      });
+
+      it('recreates the previous and next node links', () => {
+        const expectedElement = -2;
+
+        doublyLinkedList.append(10);
+        doublyLinkedList.append(100);
+        doublyLinkedList.append(expectedElement); // index 2
+        doublyLinkedList.append(50);
+
+        const expectedNode = doublyLinkedList.tail.prev;
+
+        expect(expectedNode.element).toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(4);
+
+        const removedElement = doublyLinkedList.removeAt(2);
+
+        const updatedNode = doublyLinkedList.tail.prev;
+        // Validate relink 100 <-> 50
+        expect(updatedNode.element).toBe(100);
+        expect(updatedNode.next.element).toBe(50);
+
+        expect(removedElement).toBe(expectedElement);
+        expect(doublyLinkedList.size()).toBe(3);
+      });
+    });
+  });
+
+  describe('.remove()', () => {
+    it('returns null if the element to be remove does not exists', () => {
+      expect(doublyLinkedList.remove(10)).toBe(null);
+    });
+
+    it('returns the element itself and remove from the list', () => {
+      const element = 10;
+      doublyLinkedList.append(-10);
+      doublyLinkedList.append(element);
+      doublyLinkedList.append(60);
+      expect(doublyLinkedList.toString()).toBe('-10, 10, 60');
+      expect(doublyLinkedList.size()).toBe(3);
+      expect(doublyLinkedList.remove(element)).toBe(element);
+      expect(doublyLinkedList.toString()).toBe('-10, 60');
+      expect(doublyLinkedList.size()).toBe(2);
+    });
+  });
+
+  describe('.isEmpty()', () => {
+    it('returns true if the list does not contain element', () => {
+      expect(doublyLinkedList.isEmpty()).toBe(true);
+    });
+
+    it('returns false if the list contains element', () => {
+      doublyLinkedList.append(10);
+      expect(doublyLinkedList.isEmpty()).toBe(false);
+    });
+  });
+
+  describe('.head', () => {
+    it('returns null when the list is empty', () => {
+      expect(doublyLinkedList.head).toBe(null);
+    });
+
+    it('returns the node containing the first element added', () => {
+      const element = 'my name';
+
+      doublyLinkedList.append(element);
+
+      expect(doublyLinkedList.head.element).toBe(element);
+    });
+  });
+
+  describe('.tail', () => {
+    it('is null when the list is empty', () => {
+      expect(doublyLinkedList.tail).toBe(null);
+    });
+
+    it("returns the same node as head when it's a list of a single element", () => {
+      doublyLinkedList.append(10);
+
+      expect(doublyLinkedList.tail).toStrictEqual(doublyLinkedList.head);
+    });
+
+    it('returns the last node of the list', () => {
+      const expectedElement = 'Max';
+
+      doublyLinkedList.append('Carla');
+      doublyLinkedList.append('Piet');
+      doublyLinkedList.append(expectedElement);
+
+      expect(doublyLinkedList.tail.element).toBe(expectedElement);
+    });
+  });
 });
